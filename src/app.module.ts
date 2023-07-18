325417c626b78f386ed038753ff88f0f894f3673
joa7b <joa7@protonmail.com>
2023-07-18 14:12:33 -0300

    connecting with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
+import path from 'path';
+import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { PublicModule } from './app/public/public.module';
 
+dotenv.config();
+
+const MONGODB_DATABASE = process.env.MONGODB_DATABASE || 'watermark';
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PASS = process.env.MONGODB_PASS || null;
+const MONGODB_USER = process.env.MONGODB_USER || null;
+const MONGODB_PORT = process.env.MONGODB_PORT || '27017';
+
+const connectionUrl =
+  process.env.NODE_ENV === 'production'
+    ? `mongodb://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`
+    : `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`;
+
+const options: any = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ssl = true;
+  // options.sslValidate = true;
+  options.sslCA = path.join(__dirname, '..', 'global-bundle.pem');
+}
 @Module({
-  imports: [PublicModule],
+  imports: [
+    MongooseModule.forRoot(connectionUrl, options),
+    PublicModule,
+  ],
   controllers: [],
   providers: [],
 })
