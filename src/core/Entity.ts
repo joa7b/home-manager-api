7884a2a0d11ac04c7d09d00bb4df9ed7bc650973
joa7b <joa7@protonmail.com>
2023-07-18 10:07:21 -0300

    featuring unnecessary items

---
+++ src/core/Entity.ts
@@ -0,0 +1,6 @@
+export class Entity {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+  deletedAt: Date | null;
+}
---
+++ src/domain/entities/house.entity.ts
@@ -0,0 +1,9 @@
+import { Entity } from '@core/Entity';
+
+export class House extends Entity {
+  name: string;
+  address: string;
+  residents: string[];
+  grocery_purchases: string[];
+  household_bills: string[];
+}
